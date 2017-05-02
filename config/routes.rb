28bc88cef91e4611d826bfb4c396e49376343396
William Greer <greer.184@osu.edu>
2017-05-02 08:34:44 -0400

    Rebrand to rVerify

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static#home'
   get '/home', to: 'static#help'
   get '/about', to: 'static#about'
+  get '/curation', to: 'static#curation'
   get '/signup', to: 'users#new'
   resources :users
 end
