4775adcea1cf8cf9b230df064f3129cbe6bb3535
William Greer <greer.184@osu.edu>
2017-05-06 16:43:44 -0400

    Added feeding mechanism, updated homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  root 'static#home'
-  get '/home', to: 'static#help'
+
+  # Feed Management
+  resources :feeds do
+  end
+
+  # Entries Management
+  get 'entries/index'
+  get 'entries/show'
+
+  # Homepage Management
+  root 'entries#index'
   get '/about', to: 'static#about'
   get '/curation', to: 'static#curation'
+
+  # User Management
   get '/signup', to: 'users#new'
   resources :users
 end
