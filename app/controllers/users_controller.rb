cee7c5d89202dae7bee71bdd69cd781873f8e86c
William Greer <greer.184@osu.edu>
2017-05-09 15:29:11 -0400

    Added signup capabilties

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "You have successfully created a rVerify account!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, 
+                                 :password_confirmation)
+  end
+
 end
