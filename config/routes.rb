b053a7a38bcce42d86c25eeda25d59022789cb73
William Greer <greer.184@osu.edu>
2017-05-21 20:20:50 -0400

    Started email activation

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  # Account Activation
+  resources :account_activations, only: [:edit]
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519002905) do
+ActiveRecord::Schema.define(version: 20170521235047) do
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
@@ -36,10 +36,13 @@ ActiveRecord::Schema.define(version: 20170519002905) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
