8ee40797b9b7c09c5cf49e1c1be17688a1f8987c
William Greer <greer.184@osu.edu>
2017-06-03 16:16:07 -0400

    10 tests, 10 success

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,4 +2,9 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,35 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "qwertyuiop",
+                     password_confirmation: "qwertyuiop")
   end
 
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should have name present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "should have email present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "should have valid name length" do
+    @user.name = "A" * 51
+    assert_not @user.valid?
+  end
+
+  test "should have valid email length" do
+    @user.name = "A" * 113 + "@newiphy.com"
+    assert_not @user.valid?
+  end 
+
+  test "should have valid password length" do
+  end
 end
