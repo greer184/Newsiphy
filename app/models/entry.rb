279b7bb338d725a55957c54e58c70c4d1b840b3a
William Greer <greer.184@osu.edu>
2017-06-09 15:38:17 -0400

    advanced average

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,6 +5,11 @@ class Entry < ActiveRecord::Base
   # Gives updated score for the entry
   def update_score
     score = ratings.inject(0.0){ |sum, element| sum + element.score }
-    update_attribute(:score, score / ratings.size)
+    average = score / ratings.size;
+    score = ratings.inject(0.0){ |sum, element| sum + 
+                                 (element.score - average) ** 2 }
+    penalty = Math.log10((score / ratings.size) + 1) + 1.0;
+    update_attribute(:score, average / penalty)
   end
+
 end
