5a3af1ae64a039e60d3d4349190eac0495c4310e
William Greer <greer.184@osu.edu>
2017-07-10 16:06:45 -0400

    able to make new votes

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -2,7 +2,20 @@ class ProposalsController < ApplicationController
   before_action :set_proposal, only: [:show, :edit, :update, :destroy]
 
   def index
-    @proposals = Proposal.all
+    proposals = Proposal.all
+    list = labels
+    total = User.all.count * 5.0
+    @options = []
+    list.each do |op|
+      support = 0.0
+      proposals.each do |pro|
+        if op == pro.description
+          support = support + User.find(pro.user_id).voting_power
+        end
+      end
+      percent = ((support / total) * 100).round(2)
+      @options.append([op, percent])
+    end
   end
 
   def show
@@ -10,17 +23,21 @@ class ProposalsController < ApplicationController
 
   def new
     @proposal = Proposal.new
+    @menu = labels
   end
 
   def edit
+    @menu = labels
   end
 
   def create
     @proposal = Proposal.new(proposal_params)
+    @proposal.user_id = current_user.id
     if @proposal.save
-      flash[:success] = "Proposal was successfully created"
+      flash[:success] = "Vote was successfully cast, "
+      flash[:success] += "feel free to change before deadline"
     else
-      flash[:warning] = "Proposal creation failed"
+      flash[:warning] = "Vote was not correctly logged"
     end
   end
 
@@ -43,7 +60,12 @@ class ProposalsController < ApplicationController
     @proposal = Proposal.find(params[:id])
   end
 
+  def labels
+    list = ["aardvark", "barnacle", "caterpillar", "dragonfly", "earwig"]
+  end
+
   def proposal_params
     params.require(:proposal).permit(:description)
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709024707) do
+ActiveRecord::Schema.define(version: 20170710194006) do
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170709024707) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "ratings", force: :cascade do |t|
